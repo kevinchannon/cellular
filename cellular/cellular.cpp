403074452c7bd03ebc2f83ed24ee237b3bf408fc
Kevin Channon <kevinchannon@gmail.com>
2023-02-24 19:56:21 +0000

    Add project files.

---
+++ cellular/cellular.cpp
@@ -0,0 +1,12 @@
+﻿// cellular.cpp : Defines the entry point for the application.
+//
+
+#include "cellular.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
