a5b2fc1af41d571bed359aa9d7ca270da5a7e817
Kevin Channon <kevinchannon@gmail.com>
2023-02-25 08:16:49 +0000

    Works for all rules and all parent cells in centre of 3-cell state

--- cellular/cellular.cpp
+++ cellular/cellular.cpp
@@ -15,7 +15,16 @@ namespace ca {
   }
 
   State next(State state, Rule rule) {
-    return State(state.size(), EMPTY);
+    auto out = State(state.size(), EMPTY);
+
+    static const auto filled = [](auto&& c) { return c == FILLED; };
+
+    for (auto i = 1u; i < out.size() - 1; ++i) {
+      const auto rule_bit = (filled(state[i - 1]) ? 4 : 0) + (filled(state[i]) ? 2 : 0) + (filled(state[i + 1]) ? 1 : 0);
+      out[i] = rule >> rule_bit & 0x1 ? FILLED : EMPTY;
+    }
+
+    return out;
   }
 
 }
--- cellular/cellular_tests.cpp
+++ cellular/cellular_tests.cpp
@@ -2,6 +2,9 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
+#include <catch2/generators/catch_generators_all.hpp>
+
+#include <format>
 
 TEST_CASE("next() executes rule") {
   SECTION("Rule 0 makes everything empty") {
@@ -10,11 +13,34 @@ TEST_CASE("next() executes rule") {
     const auto b = GENERATE(ca::EMPTY, ca::FILLED);
     const auto c = GENERATE(ca::EMPTY, ca::FILLED);
 
-    const auto state = ca::State{ a, b, c };
-    const auto result = ca::next(state, 0);
-
     SECTION(std::string{ a, b, c }) {
-      REQUIRE(ca::State{ ca::EMPTY, ca::EMPTY, ca::EMPTY } == result);
+      REQUIRE(ca::State{ ca::EMPTY, ca::EMPTY, ca::EMPTY } == ca::next({ a, b, c }, 0));
+    }
+  }
+
+  SECTION("Cell has the value defined by the rule and its parents") {
+    const auto [a, x] = GENERATE(table<ca::Cell, uint8_t>({
+      {ca::EMPTY,  0b000},
+      {ca::FILLED, 0b100}
+      }));
+
+    const auto [b, y] = GENERATE(table<ca::Cell, uint8_t>({
+      {ca::EMPTY,  0b000},
+      {ca::FILLED, 0b010}
+      }));
+
+    const auto [c, z] = GENERATE(table<ca::Cell, uint8_t>({
+      {ca::EMPTY,  0b000},
+      {ca::FILLED, 0b001}
+      }));
+
+    const auto rule = GENERATE(range(0, 256));
+
+    SECTION(std::format("Rule: {}, parent {}{}{}", rule, a, b, c)) {
+      const auto result = ca::next({ a, b, c }, ca::Rule(rule));
+      const auto expected = (rule >> (x | y | z)) & 0x1 ? ca::FILLED : ca::EMPTY;
+
+      REQUIRE(expected == result[1]);
     }
   }
 }
